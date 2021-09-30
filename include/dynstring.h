b949d4eac75eedd32b2ef5f31c4008161059ce08
Kuba314 <35199722+Kuba314@users.noreply.github.com>
2021-09-30 23:40:15 +0200

    Move string module documentation to header

--- include/dynstring.h
+++ include/dynstring.h
@@ -17,7 +17,35 @@ typedef struct {
     size_t length;       ///< length of the actual string
 } string_t;
 
+/**
+ * @brief Creates an empty string object
+ *
+ * @param[out] str pointer to newly created string
+ * @return 1 on allocation error, else 0
+ */
 int str_create_empty(string_t *str);
+
+/**
+ * @brief Creates a string object and loads it with a char sequence
+ *
+ * @param s char sequence to load
+ * @param[out] str pointer to newly created string
+ * @return 1 on allocation error, else 0
+ */
 int str_create(const char *s, string_t *str);
+
+/**
+ * @brief Appends a char to the string object
+ *
+ * @param str string object to append the char to
+ * @param ch char that gets appended
+ * @return 1 on allocation error, else 0
+ */
 int str_append_char(string_t *str, char ch);
+
+/**
+ * @brief Frees memory allocated by a string object
+ *
+ * @param str string to free
+ */
 void str_free(string_t *str);
