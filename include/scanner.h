128ea3dbadba448940dd8e916c899fabe77ee9f2
raspbeep <pavel4000@gmail.com>
2021-10-01 13:21:53 +0200

    Add token struct, token kind and reserved keywords implementation.

--- include/scanner.h
+++ include/scanner.h
@@ -7,27 +7,72 @@
 #include "dynstring.h"
 
 /**
- * @enum Enum of reserved keywords.
+ * @enum Definition of reserved keywords.
  */
 typedef enum {
+    // Reserved keywords for program control structure
+    KEYWORD_T_IF,
+    KEYWORD_T_ELSE,
+    KEYWORD_T_DO,
+    KEYWORD_T_END,
 
+    KEYWORD_T_FUNCTION,
+    KEYWORD_T_GLOBAL,
+    KEYWORD_T_LOCAL,
+    KEYWORD_T_NIL,
+
+    KEYWORD_T_REQUIRE,
+    KEYWORD_T_RETURN,
+    KEYWORD_T_WHILE,
+    KEYWORD_T_THEN,
+
+    // Reserved keywords for data types
+    KEYWORD_T_INTEGER,
+    KEYWORD_T_NUMBER,
+    KEYWORD_T_STRING
 } keyword_t;
 
-
-
 /**
- * @enum Union of possible token properties.
+ * @enum Definition of token_kind
  */
-typedef union {
-    string_t *string_value;
-    int int_value;
-    double double_value;
-    keyword_t keyword_value;
-} token_prop;
+typedef enum{
+    TOKEN_KIND_NONE,           // default kind?
+    TOKEN_KIND_KEYWORD,
+    TOKEN_KIND_IDENTIFIER,
+    TOKEN_KIND_EOL,
+
+    TOKEN_KIND_EOF,
+    TOKEN_KIND_BRACKET_LEFT,    // (
+    TOKEN_KIND_BRACKET_RIGHT,   // )
+    TOKEN_KIND_COMMA,           // ,
+    TOKEN_KIND_COLON,           // :
+
+    // Token for various operators
+    TOKEN_KIND_PLUS,            // +
+    TOKEN_KIND_MINUS,           // -
+    TOKEN_KIND_MUL,             // *
+    TOKEN_KIND_DIV,             // /
+    TOKEN_KIND_INT_DIV,         // //
+    TOKEN_KIND_ASSIGN,          // =
+    TOKEN_KIND_LESS,            // <
+    TOKEN_KIND_LESS_EQ,         // <=
+    TOKEN_KIND_GREATER,         // >
+    TOKEN_KIND_GREATER_EQ,      // >=
+    TOKEN_KIND_EQ,              // ==
+    TOKEN_KIND_NOT_EQ,          // ~=
+    TOKEN_KIND_HASHTAG,         // #
+    TOKEN_KIND_CONCAT           // ..
+} token_kind;
 
 /**
  * @enum Definition of token type
  */
 typedef enum {
-
+    token_kind kind;            // Kind of token. Keyword, operator, identifier, EOL, EOF...
+    union {                     // Stores dynamic string and then specific value after evaluation.
+        string_t *string_value;
+        keyword_t keyword_value;
+        int int_value;
+        double double_value;
+    };
 } token_t;
--- src/utils/scanner.c
+++ src/utils/scanner.c
@@ -5,4 +5,3 @@
 #include "scanner.h"
 
 FILE *fptr;
-
