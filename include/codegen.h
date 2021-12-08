91f579e4f85026ca73f06abbecdd3c8f99cc157a
Pavel Kratochvil <kratochvilpavel.jr@gmail.com>
2021-12-09 00:12:35 +0100

    Add codegen.h header

--- include/codegen.h
+++ include/codegen.h
@@ -1,3 +1,18 @@
+/**
+ * IFJ21 Compiler
+ *
+ *  Copyright 2021 xvanom00 Michal Vano
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ *
+ * @file codegen.h
+ *
+ * @brief IFJCode21 generator from an abstract syntax tree
+ */
+
 #include "ast.h"
 
 /**
