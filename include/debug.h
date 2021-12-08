45cca26f9768edf8cfa08fff7bdc2d1893d5f2ce
guritchi <melounlemon@gmail.com>
2021-12-09 00:28:35 +0100

    Added remaining headers

--- include/debug.h
+++ include/debug.h
@@ -1,3 +1,17 @@
+/**
+ * IFJ21 Compiler
+ *
+ *  Copyright 2021 xruzaa00 Adam Ruza
+ *
+ *  Licensed under GNU General Public License 3.0 or later.
+ *  Some rights reserved. See COPYING, AUTHORS.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ *
+ * @file debug.h
+ *
+ * @brief Debugging utilities
+ */
 #pragma once
 
 #ifdef DBG
